409da5acdd8e1ac01257276df101d4e5f2244d6c
ex-phongvt <ex.phongvt@nextbeat.net>
2018-08-08 11:17:22 +0700

    update code

--- products/models.py
+++ products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class products(models.Model):
+class Products(models.Model):
     name = models.CharField(max_length=255)
     price = models.FloatField(default=0)
     updated_at = models.DateTimeField(auto_now=True)
