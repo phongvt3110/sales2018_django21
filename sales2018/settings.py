5b63b17a5a8c5daa2994f8536f51a83052737406
ex-phongvt <ex.phongvt@nextbeat.net>
2018-09-17 16:11:45 +0700

    update code

--- sales2018/settings.py
+++ sales2018/settings.py
@@ -158,3 +158,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    ("downloads", os.path.join(BASE_DIR, "downloads"))
+]
